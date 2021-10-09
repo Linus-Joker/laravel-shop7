3ae01fb58fedb64abae66bddc04596c9163fe724
Linus_Joker <milk790513@yahoo.com.tw>
2021-10-09 16:32:07 +0800

    update model Message.

--- app/Message.php
+++ app/Message.php
@@ -6,7 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    /**
+     * 與模型關聯的資料表
+     *
+     * @var string
+     */
     protected $table = 'message';
+
+    /**
+     * 定義主键
+     *
+     * @var string
+     */
+    protected $primaryKey = 'flight_id';
+
+    /**
+     * 可以被批量賦值的属性。
+     *
+     * @var array
+     */
     protected $fillable = [
         'message_id',
         'product_id',
