f2f0fc7e547ec426620ba138a2cd4921c7a2e0b6
Linus_Joker <milk790513@yahoo.com.tw>
2022-08-06 19:22:41 +0800

    admin product page new create ajax methods.

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -15,45 +15,123 @@
             </li>
         </ol>
     </nav>
-    {{-- 主標題 --}}
-    <h4 class="text-primary">book table</h4>
-    <button id="create" class="btn btn-primary">新增</button>
 
-    <table class="table">
+    {{-- 新增 Model --}}
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Modal</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form id="addProduct">
+                    <div class="modal-body">
+                        <div id="errorMessage" class="alert alert-warning d-none"></div>
+
+                        <div class="mb-3">
+                            <label for="">product_name</label>
+                            <input type="text" name="product_name" class="form-control product_name" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="">description</label>
+                            <input type="text" name="description" class="form-control description" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="">price</label>
+                            <input type="text" name="price" class="form-control price" />
+                        </div>
+                        {{-- 還有一個產品種類沒放 --}}
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+    {{-- 主標題 --}}
+    <h4 class="text-primary">AJAX CRUD Book Product using Bootstrap Modal</h4>
+    <button id="create" class="btn btn-primary" data-toggle="modal" data-target="#addModal">
+        新增
+    </button>
+
+
+    <table id="bookTable" class="table">
         <thead>
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">product_name</th>
                 <th scope="col">description</th>
                 <th scope="col">price</th>
-                <th scope="col">更新</th>
-                <th scope="col">刪除</th> 
+                <th scope="col">操作</th>
+                
             </tr>
         </thead>
 
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>book1</td>
-                <td>123</td>
-                <td>100</td>
-                <td>
-                    <button id="update" class="btn btn-warning">修改</button>
-                </td>
-                <td>   
-                    <button id="delete" class="btn btn-danger">刪除</button>
-                </td>
-            </tr>
-        </tbody>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>book1</td>
+                    <td>123</td>
+                    <td>100</td>
+                    <td>
+                        <button id="update" class="btn btn-warning">修改</button>
+                        <button id="delete" class="btn btn-danger">刪除</button>
+                    </td>
+                </tr>
+            </tbody>
+
     </table>
 </div>
 @endsection
 
 @section('js')
 <script>
-    $(function(){
+    $(document).on('submit', '#addProduct', function (e) {
+        e.preventDefault();
 
-        //新增應該會是另外的介面，這裡給更新和刪除
+        //param
+        let name = document.querySelector('.product_name').value;
+        let description = document.querySelector('.description').value;
+        let price = document.querySelector('.price').value;
+        let products_sort_id = 1;
+
+        //formData一直傳不了，改成將資料打包好轉json，傳到後端        
+        let data = {
+            "name":name,
+            "description":description,
+            "price":price,
+            "products_sort_id":products_sort_id
+        };
+        JSON.stringify(data);
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url:'api/v1/admin/products',
+            type:'POST',
+            data:data,
+            success:function(data){
+                console.log(data);
+            }
+        });
+    });
+
+
+    $(function(){
         $("#update").click(function (e) {
             $.ajaxSetup({
                 headers: {
