e983e1ff05cbcc7cc2c446d7606b212c7189c523
Linus_Joker <milk790513@yahoo.com.tw>
2022-08-12 11:45:38 +0800

    new ProductImage Model

--- app/ProductImage.php
+++ app/ProductImage.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     protected $table = 'product_images';
+
+    protected $fillable = [
+        'image_name',
+        'image_path',
+        'products_id'
+    ];
 }
